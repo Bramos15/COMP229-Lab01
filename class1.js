01030eefb3ae504bcbfd95b5b545baa0042edd19
Bramos15 <ben.manta.15@gmail.com>
2023-05-16 13:05:51 -0400

    init

---
+++ class1.js
@@ -0,0 +1,59 @@
+class Vehicle {
+    constructor(wheels) {
+        this.wheels = wheels;
+    };
+    toString() {
+        return '(' + this.wheels + ')';
+    };
+    };
+class Car extends Vehicle {
+    constructor(color) {
+        super(4);
+        this.color = color;
+    };
+    toString() {
+        return super.toString() + ' colored: ' + this.color;
+    };
+    };
+let car = new Car('blue');
+car.toString();
+console.log(car instanceof Car);
+console.log(car instanceof Vehicle);
+
+const sum1 = (a, b) => {
+    return a + b;
+};
+let result = sum1(1, 2);
+console.log(result);
+
+function reinitConst(){
+    const me = 1;
+    console.log(me);
+    me = 2;
+    console.log(me);
+};
+reinitConst();
+
+function reinitConst(){
+    let me2 = 1;
+    console.log(me2);
+    me2 = 2;
+    console.log(me2);
+};
+reinitConst();
+
+function iterateVar() {
+    for(var i = 0; i < 10; i++) {
+        console.log(i);
+    };
+    console.log(i);
+};
+iterateVar();
+
+function iterateLet() {
+    for(let i = 0; i < 10; i++) {
+        console.log(i);
+    };
+    console.log(i);
+};
+iterateLet();
